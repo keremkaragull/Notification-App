7732798b76c31a3ce4a09e175b8297179d4dfccd
Kerem Karagül <karagulkeremis@gmail.com>
2023-11-28 17:11:41 +0300

    Programme completed

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+notification.notify(
+    title = 'Kahve Saati',
+    message = 'Bir fincan kahve içme zamanı.',
+    app_icon = 'output.ico',
+    timeout = 10,
+)
